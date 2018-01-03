a7183f8714f1c0cfb662344994e427419ec7d119
Jesper Sørensen <jesper@infoserv.dk>
2018-01-03 10:17:19 +0100

    update fix

--- update-plugin.php
+++ update-plugin.php
@@ -8,14 +8,14 @@ function github_plugin_updater_test_init() {
             'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
             'proper_folder_name' => 'popup-infoserv', // this is the name of the folder your plugin lives in
             'api_url' => 'https://github.com/martinskou/popup-infoserv', // the GitHub API url of your GitHub repo
-            'raw_url' => 'https://raw.githubusercontent.com/martinskou/popup-infoserv/master/init.php?token=f5a7cb4fc60b770ce93d3779c8038a12126424c3', // the GitHub raw url of your GitHub repo
+            'raw_url' => 'https://raw.githubusercontent.com/martinskou/popup-infoserv/master/init.php?token=46eecc40c2f76fb6b1563eea295ef63837fb3245', // the GitHub raw url of your GitHub repo
             'github_url' => 'https://github.com/martinskou/popup-infoserv', // the GitHub url of your GitHub repo
             'zip_url' => 'https://github.com/martinskou/popup-infoserv/archive/master.zip', // the zip url of the GitHub repo
             'sslverify' => false, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
             'requires' => '3.0', // which version of WordPress does your plugin require?
             'tested' => '3.3', // which version of WordPress is your plugin tested up to?
             'readme' => 'README.md', // which file to use as the readme for the version number
-            'access_token' => 'f5a7cb4fc60b770ce93d3779c8038a12126424c3', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+            'access_token' => '46eecc40c2f76fb6b1563eea295ef63837fb3245', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
         );
         new WP_GitHub_Updater($config);
     }
