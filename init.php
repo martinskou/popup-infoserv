16ba1da67c07bfecdee69b41b3372b3d85184f37
Jesper Sørensen <jesper@infoserv.dk>
2018-01-03 13:55:21 +0100

    disabling token

--- init.php
+++ init.php
@@ -26,10 +26,10 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 );
 
 //Optional: If you're using a private repository, specify the access token like this:
-$myUpdateChecker->setAuthentication('your-token-here');
+//$myUpdateChecker->setAuthentication('your-token-here');
 
 //Optional: Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('stable-branch-name');
+//$myUpdateChecker->setBranch('stable-branch-name');
 
 add_action('plugins_loaded', 'pui_load_textdomain');
 
